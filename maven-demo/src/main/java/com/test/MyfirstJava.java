50aae47b15dc810ca2d260a0d2bcedac6d31a3be
raopa <raopa@Koti-Surface.lan1>
2020-07-12 14:49:35 -0400

    test

--- maven-demo/src/main/java/com/test/MyfirstJava.java
+++ maven-demo/src/main/java/com/test/MyfirstJava.java
@@ -3,7 +3,7 @@ package com.test;
 public class MyfirstJava {
 
 	public static void main(String[] args) {
-		System.out.println("First java Program1");
+		System.out.println("First java Program2");
 
 	}
 
