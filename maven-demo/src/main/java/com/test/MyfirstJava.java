bf8226c1ca800476e0c0f28cc6112787d83011aa
raopa <raopa@192.168.1.241>
2020-07-11 20:50:31 -0400

    Second commit

--- maven-demo/src/main/java/com/test/MyfirstJava.java
+++ maven-demo/src/main/java/com/test/MyfirstJava.java
@@ -3,7 +3,7 @@ package com.test;
 public class MyfirstJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!,");
+		System.out.println("First java Program");
 
 	}
 
