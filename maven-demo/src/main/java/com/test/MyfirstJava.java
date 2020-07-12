60526799c95afdcdfb2644adedc023680d6056e1
raopa <raopa@Koti-Surface.lan1>
2020-07-12 14:52:04 -0400

    My third commit

--- maven-demo/src/main/java/com/test/MyfirstJava.java
+++ maven-demo/src/main/java/com/test/MyfirstJava.java
@@ -3,7 +3,7 @@ package com.test;
 public class MyfirstJava {
 
 	public static void main(String[] args) {
-		System.out.println("First java Program2");
+		System.out.println("First java Program3");
 
 	}
 
